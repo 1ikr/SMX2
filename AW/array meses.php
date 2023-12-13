400c8fca9a14d520dde349868502a47d734b624e
1ikr <146021504+1ikr@users.noreply.github.com>
2023-12-13 18:09:34 +0100

    array meses.php

---
+++ AW/array meses.php
@@ -0,0 +1,10 @@
+<?php
+$meses = array
+ (
+"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"
+);
+for ($i = 0; $i < count($meses); $i++)
+{
+echo $meses [$i], "<br>";
+}
+?>
